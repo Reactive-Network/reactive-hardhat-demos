757c44eb224f09de3f7ba3d44265fb94eaa8706d
konstantin <konstantin@parsiq.net>
2025-07-03 17:58:50 +0700

    edited TokenTurnoverL1Module

--- ignition/modules/erc20-turnovers/TokenTurnoverL1Module.js
+++ ignition/modules/erc20-turnovers/TokenTurnoverL1Module.js
@@ -9,7 +9,7 @@ const TokenTurnoverL1Module = buildModule("TokenTurnoverL1Module", (m) => {
     }
 
     const turnoverContract = m.contract("TokenTurnoverL1", [callbackSender], {
-        value: 10000000000000000n, // 0.01 ether
+        value: 20000000000000000n, // 0.02 ether
     });
 
     return { turnoverContract };
