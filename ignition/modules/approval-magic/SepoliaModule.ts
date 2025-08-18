899f86448a35a2878df41feb8be7fdb1c6c17ccc
konstantin <konstantin@parsiq.net>
2025-08-18 15:07:13 +0700

    added after to dependent contracts

--- ignition/modules/approval-magic/SepoliaModule.ts
+++ ignition/modules/approval-magic/SepoliaModule.ts
@@ -10,35 +10,44 @@ const SepoliaModule = buildModule("SepoliaModule", (m) => {
         "ApprovalService",
         [callbackProxy, subscriptionFee, gasCoefficient, extraGas],
         {
-            value: 20000000000000000n, // 0.02 ether
+            value: 2000000000000000n, // 0.002 ether
         }
     );
 
     const exchangeToken = m.contract(
         "ApprovalDemoToken",
         ["FTW", "FTW"],
-        { id: "ExchangeToken" });
+        { id: "ExchangeToken" }
+    );
 
     const swapToken1 = m.contract(
         "ApprovalDemoToken",
         ["TK1", "TK1"],
-        { id: "SwapToken1" });
+        { id: "SwapToken1" }
+    );
 
     const swapToken2 = m.contract(
         "ApprovalDemoToken",
         ["TK2", "TK2"],
-        { id: "SwapToken2" });
+        { id: "SwapToken2" }
+    );
 
     const exchangeContract = m.contract(
         "ApprovalEthExch",
         [approvalService, exchangeToken],
-        { value: 10000000000000000n } // 0.01 ether
+        {
+            value: 1000000000000000n, // 0.001 ether
+            after: [approvalService, exchangeToken],
+        }
     );
 
     const swapContract = m.contract(
         "ApprovalMagicSwap",
         [approvalService, swapToken1, swapToken2],
-        { value: 10000000000000000n } // 0.01 ether
+        {
+            value: 1000000000000000n, // 0.001 ether
+            after: [approvalService, swapToken1, swapToken2],
+        }
     );
 
     return { approvalService, exchangeToken, swapToken1, swapToken2, exchangeContract, swapContract };
